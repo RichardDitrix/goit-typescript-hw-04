fb8a97b793211bf1cb6d440df44705e18115e232
Vladimir Tsymbaliuk <89538231+RichardDitrix@users.noreply.github.com>
2024-02-25 17:51:09 +0200

    1

---
+++ src/homework/1.tsx
@@ -0,0 +1,42 @@
+import React, { ReactNode, useEffect, useRef } from "react";
+
+type ObserverProps = {
+  children: ReactNode;
+  onContentEndVisible: () => void;
+};
+
+export function Observer({ children, onContentEndVisible }: ObserverProps) {
+  const endContentRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    const options: IntersectionObserverInit = {
+      rootMargin: "2px",
+      threshold: 1.0,
+      root: null,
+    };
+
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.intersectionRatio > 0) {
+          onContentEndVisible();
+          observer.disconnect();
+        }
+      });
+    }, options);
+
+    if (endContentRef.current) {
+      observer.observe(endContentRef.current);
+    }
+
+    return () => {
+      observer.disconnect();
+    };
+  }, [onContentEndVisible]);
+
+  return (
+    <div>
+      {children}
+      <div ref={endContentRef} />
+    </div>
+  );
+}
